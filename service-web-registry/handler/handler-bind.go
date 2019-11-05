ae05ef236567054c6dac580b94b34e59de5bab3b
wuxun <84924650@qq.com>
2019-11-05 22:01:25 +0800

    拆分util，使代码清晰

--- service-web-registry/handler/handler-bind.go
+++ service-web-registry/handler/handler-bind.go
@@ -3,12 +3,7 @@
 package handler
 
 import (
-	"errors"
 	"fmt"
-	"github.com/micro/go-micro/config"
-	"github.com/micro/go-micro/config/encoder/json"
-	"github.com/micro/go-micro/config/source"
-	"github.com/micro/go-micro/config/source/file"
 	"github.com/micro/go-micro/web"
 	"handlerManageTest/myregistry"
 )
@@ -22,49 +17,4 @@ func BindHandlerFromConf(service web.Service, configPath string){
 		fmt.Println("f ------> url  :  ", fName, url)
 		myregistry.BindUrlHandle(service, url.(string), fName.(string))
 	}
-}
-
-//获取字典中的值
-func GetMapContent(m map[string]interface{}, path ...string) (interface{}, error){
-	//本接口将获取一个map中，按path路径取值，返回一个interface
-	var content interface{}
-	var ok bool
-	l := len(path)
-	if l ==0 || (l == 1 && path[0]==""){  //当没有填入
-		return m, nil
-	}
-	for k, v:= range path{
-		if k ==l-1{
-			content, ok = m[v]
-			if !ok{
-				return nil, errors.New(" 配置读取错误---> 	" + v)
-			}
-			return content,nil
-		}
-		if m, ok = m[v].(map[string]interface{}); !ok{
-			return nil, errors.New(" 配置读取错误---> 	" + v)
-		}
-	}
-	return nil, errors.New("missing map!")
-}
-
-//指定文件中的配置
-func ReadConfig(filePath string)(map[string]interface{}, error){
-	configPath := filePath
-	e := json.NewEncoder()
-	fileSource := file.NewSource(
-		file.WithPath(configPath),
-		source.WithEncoder(e),
-	)
-	conf := config.NewConfig()
-	// 加载micro.yml文件
-	if err := conf.Load(fileSource); err != nil {
-		panic(err)
-	}
-	routes := make(map[string]interface{})
-	err := conf.Scan(&routes)
-	if err != nil{
-		return nil, err
-	}
-	return routes, nil
 }
