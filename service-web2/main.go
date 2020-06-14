f1dd7c9cf9ca393955f35ddc5e79e5bd30c21364
LiuBaiSMD <84924650@qq.com>
2020-06-14 15:32:22 +0800

    tszt

--- service-web2/main.go
+++ service-web2/main.go
@@ -10,6 +10,7 @@ func main() {
 	// 设置路由，如果访问/，则调用index方法
 	http.Handle("/css/", http.FileServer(http.Dir("html")))
 	http.Handle("/js/", http.FileServer(http.Dir("html")))
+	http.Handle("/flac/", http.FileServer(http.Dir("html")))
 	http.HandleFunc("/", love)
 
 	// 启动web服务，监听9090端口
